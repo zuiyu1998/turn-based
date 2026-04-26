f47b5a7dcda0fee7663a26d61727c518d46c89ec
zuiyu1998 <1542844298@qq.com>
2026-04-26 13:28:33 +0800

    feat: 添加高亮格子

--- src/level/game_area.gd
+++ src/level/game_area.gd
@@ -2,6 +2,23 @@ extends Node2D
 class_name GameArea
 
 @onready var grass: TileMapLayer = $Grass
+@onready var highlighted_line: HighlightedLine = $HighlightedLine
+
+
+func compute_highlighted():
+	var mouse = get_local_mouse_position()
+	var position = grass.local_to_map(mouse)
+	set_highlighted(position)
+
+func _process(delta: float) -> void:
+	compute_highlighted()
+
+
+# 设置高亮
+func set_highlighted(position: Vector2i):
+	var local = grass.map_to_local(position)
+	highlighted_line.position = local
+
 
 func get_local_position(position: Vector2i) -> Vector2:
 	var local = grass.map_to_local(position)
